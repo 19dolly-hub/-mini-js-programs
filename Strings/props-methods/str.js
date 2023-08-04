22162c9477b5e53f5b98319c1c3da8a799dc1910
dolly <dollyraghuwanshi16@gmail.com>
2023-08-04 16:35:17 +0530

    string new additions

--- Strings/props-methods/str.js
+++ Strings/props-methods/str.js
@@ -6,3 +6,17 @@ function props(str) {
     return result;
 }
 
+function firstWord(str) {
+    let words = str.trim();
+    let space = words.indexOf(' ');
+    
+    let result = words.substr(0, space);
+    return result;
+}
+
+function normalize(str) {
+    let norm = str.replace('-', '/');
+    let newNorm = norm.replace('-', '/');
+
+    return newNorm;
+}
