fa43004c7a8644f08c17ad2654566083dc7f6158
dolly <dollyraghuwanshi16@gmail.com>
2023-08-27 21:09:35 +0530

    initialize

--- Strings/indexOf/index.js
+++ Strings/indexOf/index.js
@@ -1,7 +1,27 @@
-function secondIndexOf(s1, s2) {
+function secondIndexOf(s2, s1) {
     let firstIndex = s1.indexOf(s2);
     let secondIndex = s1.indexOf(s2, firstIndex + 1);
     return secondIndex;
+}
+
+function index(str, fullString) {
+    const indices = [];
+    const strings = [];
+    let currentIndex = fullString.indexOf(str);
+    for(let i=0; i<fullString.split(" ").length; i++) {
+        let nextIndex = fullString.indexOf(str, currentIndex + 1);
+        indices.push(currentIndex);
+        strings.push(fullString.substr(currentIndex, 4));
+        currentIndex = nextIndex;
+    }
+    console.log(indices, strings);
+}
+
+function replaceIn(full) {
+    for(let i=0; i<3; i++) {
+        let newFull = full.replace("the", "a");
+        full = newFull;
+    }
 }
 
 function normalize(str) {
@@ -9,4 +29,27 @@ function normalize(str) {
     let newNorm = norm.replace('-', '/');
 
     return newNorm;
+}
+
+const inputText = document.getElementById("input-text");
+const searchWordInput = document.getElementById("search-word");
+const searchButton = document.getElementById("search-button");
+const resultOutput = document.getElementById("result");
+
+searchButton.addEventListener("click", () => {
+  const fullString = inputText.value;
+  const searchWord = searchWordInput.value;
+
+  const searchIndex = fullString.indexOf(searchWord);
+  
+  if (searchIndex !== -1) {
+    const replacedString = replaceAt(fullString, searchIndex, searchWord.length, `[${searchWord}]`);
+    resultOutput.textContent = replacedString;
+  } else {
+    resultOutput.textContent = "Search word not found in text.";
+  }
+});
+
+function replaceAt(text, index, length, replacement) {
+  return text.substring(0, index) + replacement + text.substring(index + length);
 }
