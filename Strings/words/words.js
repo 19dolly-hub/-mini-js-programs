5c845e707c793b4e84b0fbcadf81ad07a4c73028
dolly <dollyraghuwanshi16@gmail.com>
2023-08-27 16:35:06 +0530

    modify

--- Strings/words/words.js
+++ Strings/words/words.js
@@ -7,18 +7,28 @@ const abbr = document.getElementById("abbr");
 const msg = document.getElementById("msg-alert");
 const previewBox = document.getElementById("box");
 
+
+//--------------------
+// THE MAIN FUNCTION 
+//--------------------
+
+
+
 inputField.addEventListener("input", (e) => {
     e.preventDefault();
 
+    // Property 1 : .length | Method 1: .trim()
     const input = inputField.value.trim();
     letterCount.innerText = input.length;
 
     const words = input.split(" ");
     wordCount.innerText = words.length;
     
+    // Method 2: .toUpperCase() | Method 3: .toLowerCase()
     uppercase.innerText = input.toUpperCase();
     lowercase.innerText = input.toLowerCase();
 
+    // Method 4: .charAt()
     const abbrArr = [];
     words.forEach((string) => {
         const firstLetter = string.charAt(0).trim();
@@ -33,5 +43,10 @@ inputField.addEventListener("input", (e) => {
         msg.innerText = "";
     }
 
+    // Method 5: .substr() etc.
     previewBox.innerText = input.substr(0, 30) + "...";
 });
+
+
+//--------------------
+//--------------------
